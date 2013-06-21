ef2755dba32b9e6d8d1f9d2debb02bbff11917d7
Abishek Bhat <bhat.abishek@gmail.com>
2013-06-22 01:10:45 +0530

    Added examples to use Plivo XML

--- examples/xml.rb
+++ examples/xml.rb
@@ -1,10 +1,19 @@
 require_relative '../lib/plivo.rb'
-include Plivo
+include Plivo::XML
 
-response = Plivo::XML::Response.new
-
+response =XML::Response.new
+##Direct Dial 
 response.Dial 'action'=>'foobar','method'=>'GET' do|number| 
     number.Number body='919940728522','sendDigits'=>true 
 end
 
-print response.to_xml()
+#Creating a conference
+conference_response=XML::Response.new
+
+conference_response.Conference "Plivo",'method'=>'GET','startConferenceOnEnter'=>true,'record'=>true
+
+XML::Response.new do |parent|
+	parent.GetDigits 'action'=>'example.com/action_url','method'=>'POST','retries'=>2 do |child|
+		child.Speak "Say something"
+	end
+end
