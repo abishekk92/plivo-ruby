c95d2c299c6c075383f6166921784979c93ade0b
abishekk92 <bhat.abishek@gmail.com>
2013-07-17 23:42:38 +0530

    Fixing includes

--- examples/xml.rb
+++ examples/xml.rb
@@ -1,18 +1,18 @@
 require_relative '../lib/plivo.rb'
 include Plivo::XML
 
-response =XML::Response.new
+response = Response.new
 ##Direct Dial 
 response.Dial 'action'=>'foobar','method'=>'GET' do|number| 
     number.Number body='919940728522','sendDigits'=>true 
 end
 
 #Creating a conference
-conference_response=XML::Response.new
+conference_response = Response.new
 
 conference_response.Conference "Plivo",'method'=>'GET','startConferenceOnEnter'=>true,'record'=>true
 
-XML::Response.new do |parent|
+Response.new do |parent|
 	parent.GetDigits 'action'=>'example.com/action_url','method'=>'POST','retries'=>2 do |child|
 		child.Speak "Say something"
 	end
