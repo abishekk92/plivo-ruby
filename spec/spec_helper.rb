f2c0569e679147bfbb502c5041d6b3da68943e46
abishekk92 <bhat.abishek@gmail.com>
2013-07-29 14:57:06 +0530

    Setting up structure for test with rspec,webmock and VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,23 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'plivo'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+#Turn config
+Turn.config do |c|
+ c.format  = :outline
+ c.trace   = true
+ c.natural = true
+end
+ 
+#VCR config
+VCR.config do |c|
+  c.cassette_library_dir = 'spec/fixtures/plivo_cassettes'
+  c.stub_with :webmock
+end
 
 RSpec.configure do |config|
   
