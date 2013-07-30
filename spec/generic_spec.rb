4705c6e02c663d569c3bf8a86fdce8c1257e1123
abishekk92 <bhat.abishek@gmail.com>
2013-07-30 14:58:24 +0530

    Using VCR.use_cassette in the existing specs

--- spec/generic_spec.rb
+++ spec/generic_spec.rb
@@ -1,20 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Generic" do
-	before do
+    before do
 
-		@client=Plivo.authenticate 'some_id','some_token'
-	end
-	it "should set auth_id and auth_ token " do
-		@client.should_not be_instance_of(NilClass)
-	end
+        @client=Plivo.authenticate 'MAMWY3MWMZZWI5YWQ5N2','MTM1YTM3Y2FiZjkxY2UwOTI2MDQwM2JhOTQ1YTg0'
+    end
+    it "should set auth_id and auth_ token " do
+        VCR.use_cassette 'authenticate' do
+            @client.should_not be_instance_of(NilClass)
+        end
+    end
 
-	it "should set api_extension" do
-		@client::Call.instance_variable_get(:@api_extension).should=="/Call"
-	end
+    it "should set api_extension" do
+        @client::Call.instance_variable_get(:@api_extension).should=="/Call/"
+    end
 
-	it "should create a sub extension" do
-		@client::SubAccount.create_subextension("12245","acvds").should=="/Subaccount/12245/acvds"
-	end
+    it "should create a sub extension" do
+        @client::SubAccount.create_subextension("12245","acvds").should=="/Subaccount/12245/acvds"
+    end
 end
 
