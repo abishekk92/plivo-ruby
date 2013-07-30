4ba50300876df5fe20035542994ab134424e05fa
abishekk92 <bhat.abishek@gmail.com>
2013-07-30 14:58:01 +0530

    Refactoring VCR config and adding color to rspec spec outputs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rspec'
+require 'turn'
+require 'vcr'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
+
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 #Turn config
@@ -14,11 +18,11 @@ Turn.config do |c|
 end
  
 #VCR config
-VCR.config do |c|
+VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/plivo_cassettes'
-  c.stub_with :webmock
+  c.hook_into :webmock
 end
 
 RSpec.configure do |config|
-  
+  config.color_enabled = true  
 end
